b77c1ee8f0f507d272b622a1e86468f362f17320
Nico <5westbury5@gmail.com>
2017-09-17 10:02:32 +0000

    added cart, changed database, added small callbacks

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 import unirest, json, uuid
-from models import Account, Item
+from models import Session, Account, Item
+from OpenSSL import SSL
 
 app = Flask(__name__)
+cart_itemse = {}
+session = Session()
 
 ################
 ### API Keys ###
@@ -19,14 +22,8 @@ square_access_token = 'sandbox-sq0atb-VGm6YqbVmufgp9MHE3j1FQ'
 @app.route('/')
 @app.route('/seller')
 def seller():
-	# revenue = calculateRevenue()
-	def callback(session):
-		return render_template("dashboard.html", items=session.query(Item).order_by(Item.id.asc()).all())
-
-# def calculateRevenue():
-# 	revenue = 0
-	
-# 	return revenue
+        items = session.query(Item).order_by(Item.id.asc()).all()
+	return render_template("dashboard.html", items=items)
 
 # Display buyer page to connect accounts for Coinbase and Square, see history/info/etc
 @app.route('/buyer')
@@ -36,9 +33,34 @@ def buyer():
 # Display item info
 @app.route('/item/<int:item_id>')
 def display_item_info(item_id):
-	#item_name = Item.query() # (SELECT name FROM items WHERE id = item_id);
-	item_price = "$" + str(35.99)
-	return render_template("table.html", item_name="Chair", item_price=item_price)
+	item = session.query(Item).filter(Item.id == item_id).first()
+        if item is None:
+                return 'No item found.'
+	return render_template("table.html", item_name=item.name, item_price=item.price)
+
+@app.route('/reset')
+def reset():
+        cart_items = {}
+
+@app.route('/add/<int:item_id>', methods=['POST'])
+def add():
+        if item_id in cart_items:
+                cart_items[item_id]['count'] += 1
+        else:
+                cart_items[item_id] = {'count': 1}
+        return 'All good in the hood! :)'
+
+@app.route('/remove/<int:item_id>', methods=['POST'])
+def remove():
+        if item_id in cart:
+                cart_items[item_id]['count'] = min(0, cart_items[item_id]['count'])
+        return 'OK'
+
+@app.route('/cart')
+def cart():
+        items = [{"total": cart_items[i.id]['count'] * i.price, "count": cart_items[i.id]['count'],
+                  "name": i.name, "price": i.price} for i in session.query(Item).all() if i.id in cart_items]
+        return render_template("cart.html", items=items)
 
 # Execute payment through Square API
 @app.route('/pay/square/<int:item_id>', methods=['GET','POST'])
@@ -57,8 +79,8 @@ def square(item_id):
 		params = json.dumps({
 			'card_nonce': card_nonce,
 			'amount_money': {
-				'amount': 100,
-				'currency': 'USD'
+			        'amount': 100,
+			        'currency': 'CAD'
 			},
 			'idempotency_key': str(uuid.uuid1())
 		})
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, BigInteger, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -11,7 +11,7 @@ class Account(Base):
     __tablename__ = 'accounts'
     id = Column(Integer, primary_key=True)
     #balance = Column(Integer)
-    card = Column(Integer)
+    card = Column(String)
 
 # The Items class corresponds to the "items" database table.
 class Item(Base):
@@ -24,32 +24,36 @@ class Item(Base):
 
 # Create an engine to communicate with the database. The "cockroachdb://" prefix
 # for the engine URL indicates that we are connecting to CockroachDB.
-engine = create_engine("cockroachdb://FourthWall@localhost:26257/myStore?sslmode=disable")
+engine = create_engine("postgres://test:test@localhost/mystore?sslmode=disable")
 Session = sessionmaker(bind=engine)
 
 # Automatically create the "accounts" and "items" tables based on the Account and Item classes.
 Base.metadata.create_all(engine)
 
-# Insert two rows into the "accounts" table.
-session = Session()
-session.add_all([
-    Account(id=1, card=4532759734545858 ),	# Visa
-    Account(id=2, card=5409889944179029),	# MasterCard
-    Account(id=3, card=6011033621379697),	# Discover
-    Account(id=4, card=36004244846408),		# Diners Club
-    Account(id=5, card=3566005734880650),	# JCB
-    Account(id=6, card=371263462726550),	# American Express
-    Account(id=7, card=6222520119138184),	# China UnionPay
-    Item(id=1, name="Chair", count=20, sold=0, price=39.99),
-    Item(id=2, name="Table", count=10, sold=0, price=89.99),
-    Item(id=3, name="Lamp", count=10, sold=0, price=25.99),
-])
-session.commit()
-
-# Print out the accounts
-for account in session.query(Account):
-    print(account.id, account.card)
-
-# Print out the items
-for item in session.query(Item):
-    print(item.id, item.name, item.count, item.sold, item.price)
+first = True
+if first:
+    # Insert two rows into the "accounts" table.
+    session = Session()
+    """session.add_all([
+        Account(id=1, card="4532759734545858" ),# Visa
+        Account(id=2, card="5409889944179029"),	# MasterCard
+        Account(id=3, card="6011033621379697"),	# Discover
+        Account(id=4, card="36004244846408"),	# Diners Club
+        Account(id=5, card="3566005734880650"),	# JCB
+        Account(id=6, card="371263462726550"),	# American Express
+        Account(id=7, card="6222520119138184"),	# China UnionPay
+    ])
+    session.add_all([
+        Item(id=1, name="Chair", count=20, sold=0, price=39.99),
+        Item(id=2, name="Desk", count=10, sold=0, price=89.99),
+        Item(id=3, name="Table", count=10, sold=0, price=55.99),
+    ])
+    session.commit()"""
+    
+    # Print out the accounts
+    for account in session.query(Account):
+        print(account.id, account.card)
+        
+    # Print out the items
+    for item in session.query(Item):
+        print(item.id, item.name, item.count, item.sold, item.price)
